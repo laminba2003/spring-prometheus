5eaa56e373a03aa7a1bda74de84d1a16abff12ff
mamadou lamine ba <laminba2003@gmail.com>
2022-01-24 21:26:35 +0100

    initial commit

--- src/main/java/com/spring/training/prometheus/Application.java
+++ src/main/java/com/spring/training/prometheus/Application.java
@@ -1,13 +1,23 @@
 package com.spring.training.prometheus;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner run(MeterRegistry meterRegistry) throws Exception {
+		return args -> {
+			meterRegistry.counter("customers","type","vip").increment(50);
+		};
 	}
 
 }
--- src/test/java/com/spring/training/prometheus/ApplicationTests.java
+++ src/test/java/com/spring/training/prometheus/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PrometheusApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
